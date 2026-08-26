7c6872ff7f56afbff9afac20b43919d1076823ef
Igor Henrique <igorj2henrique@gmail.com>
2026-08-26 19:51:30 -0300

    :sparkles: Adiciona tela de login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,9 @@
+import { Login } from './pages/login';
+
 function App() {
   return (
-    <div className="min-h-screen bg-slate-900 text-white flex items-center justify-center">
-      <div className="p-8 bg-slate-800 rounded-2xl shadow-xl border border-slate-700 text-center">
-        <h1 className="text-3xl font-extrabold text-blue-400 mb-2">
-          Gerenciador de Tarefas
-        </h1>
-        <p className="text-slate-400">
-          Tailwind CSS v4 está ativo e funcionando perfeitamente!
-        </p>
-      </div>
-    </div>
+    <Login />
   )
 }
 
-export default App
+export default App;
